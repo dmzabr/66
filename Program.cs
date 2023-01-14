58c0660555d313bfef63471fdfb701d51113cf5c
dmzabr <119075451+dmzabr@users.noreply.github.com>
2023-01-14 20:01:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите первое число");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int M = Convert.ToInt32(Console.ReadLine());
+
+if (N > M)
+{
+    (M, N) = (N, M);
+}
+
+int sum = 0;
+
+Console.WriteLine(Function(N, M, sum));
+
+int Function(int n, int m, int sum)
+{
+    sum = sum+n;
+    if (n == m)
+        return sum;
+    return Function(n + 1, m, sum);
+}
+
+Console.Read();
